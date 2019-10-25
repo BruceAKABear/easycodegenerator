cbf35d35ac52b7bd97e6debd4c3c70925436cc83
dengyi <dengyi@dengyi.pro>
2019-10-25 13:02:58 +0800

    测试@slf4j

--- src/test/java/pro/dengyi/easycodegenerator/EasycodegeneratorApplicationTests.java
+++ src/test/java/pro/dengyi/easycodegenerator/EasycodegeneratorApplicationTests.java
@@ -1,13 +1,15 @@
 package pro.dengyi.easycodegenerator;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class EasycodegeneratorApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+  @Test
+  void contextLoads() {
+    log.info("hello:{}", "world");
+  }
 }
